6f8360b2d5c18ed80df1c3038473c9b8aa67c366
PGCP-AI <mulaniashish16@gmail.com>
2026-03-23 14:56:35 +0530

    new

--- Day5/src/Collection/TestHashSet.java
+++ Day5/src/Collection/TestHashSet.java
@@ -1,16 +1,24 @@
 package Collection;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 public class TestHashSet {
     public static void main(String[] args) {
 
-        HashSet<Integer> hashset = new HashSet<>();
+        //HashSet<Integer> hashset = new HashSet<>();                   //unordered array- prints randomly
+        //LinkedHashSetTreeSet<Integer> hashset = new TreeSet<>();      //sorted array
+        LinkedHashSet<Integer> hashset = new LinkedHashSet<>();         //ordered array-prints sequentially
 
         hashset.add(150);
         hashset.add(20);
         hashset.add(450);
+        hashset.add(350);
+        hashset.add(450);  //no duplicate
         System.out.println(hashset);
 
+        hashset.remove(20);
+        System.out.println(hashset);
     }
 }
